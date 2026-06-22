b2d3ebb0c0e01266cb6b4b2d6ce2356e350836c0
galadriellagoguadze <galadriellamedeiros999@gmail.com>
2026-06-22 15:59:47 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -1,6 +1,8 @@
 package prwtemp;
 
+
 import java.util.HashMap;
+
 
 public class Sistema {
 
@@ -11,8 +13,22 @@ public class Sistema {
         this.restaurantes = new HashMap<>();
         this.clientes = new HashMap<>();
     }
+    
+    
 
-    public boolean adicionarRestaurante(String nome, String prato) {
+    public HashMap<String, Restaurante> getRestaurantes() {
+		return restaurantes;
+	}
+
+
+
+	public HashMap<String, Cliente> getClientes() {
+		return clientes;
+	}
+
+
+
+	public boolean adicionarRestaurante(String nome, String prato) {
     	if(restaurantes.get(nome) == null) {
     		Restaurante r = new Restaurante(nome, prato);
     		restaurantes.put(nome, r);
@@ -24,7 +40,7 @@ public class Sistema {
     public boolean adicionarCliente(String nome, String email) {
     	if(clientes.get(email) == null) {
     		Cliente c = new Cliente(nome, email);
-    		clientes.put(nome, c);
+    		clientes.put(email, c);
     		return true;
     	}
         return false;
@@ -33,35 +49,74 @@ public class Sistema {
     public void visitarRestaurante(String emailCliente, String nomeRestaurante) {
     	Restaurante r = restaurantes.get(nomeRestaurante);
     	Cliente c = clientes.get(emailCliente);
+    	
+    	Visita v = new Visita(c);
+    	r.registrarVisita(v);
+    	r.adicionarCliente(c);
 
     }
 
     public void visitarRestaurante(String emailCliente, String nomeRestaurante, String comentario) {
-
+    	Restaurante r = restaurantes.get(nomeRestaurante);
+    	Cliente c = clientes.get(emailCliente);
+    	
+    	Visita v = new Visita(c, comentario);
+    	r.registrarVisita(v);
+    	r.adicionarCliente(c);
     }
 
     public double obtemGastoCliente(String emailCliente) {
-        return 0;
+    	Cliente c = clientes.get(emailCliente);
+    	
+        return c.getGasto();
     }
 
     public String[] listarClientesRestaurante(String nomeRestaurante) {
-        return new String[0];
+    	Restaurante r = restaurantes.get(nomeRestaurante);
+    	
+        return r.listarClientes();
     }
 
     public String votarPratoPreferido(String emailCliente, String nomeRestaurante) {
-        return "";
+    	Restaurante r = restaurantes.get(nomeRestaurante);
+    	Cliente c = clientes.get(emailCliente);
+    	
+    	if(r.clienteVisitou(c)) {
+    		r.receberVoto();
+    		return "Voto registrado com sucesso!";
+    	}
+        return "Voto não registrado";
     }
 
     public String melhorPrato() {
-        return "";
+    	Restaurante melhorRestaurante = null; 
+    	for(Restaurante r : restaurantes.values()) {
+    		if(melhorRestaurante == null || r.getVotos() > melhorRestaurante.getVotos()) {
+    			melhorRestaurante = r;
+    		}
+    	}
+        return "Melhor prato: " + melhorRestaurante.getPrato() + " Restaurante: " + melhorRestaurante.getNome();
     }
 
     public String[] listarLivroVisitas(String nomeRestaurante) {
-        return new String[0];
+    	Restaurante r = restaurantes.get(nomeRestaurante);
+    	
+        return r.listarVisitas();
     }
 
     public String[] listarLivroVisitas(String nomeRestaurante, int numRecentes) {
-        return new String[0];
+    	Restaurante r = restaurantes.get(nomeRestaurante);
+        return r.listarUltimasNVisitas(numRecentes);
     }
+
+
+
+	public Restaurante getRestaurante(String string) {
+		return restaurantes.get(string);
+	}
+	
+	public Cliente getCliente(String string) {
+		return clientes.get(string);
+	}
 
 }

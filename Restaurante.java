92abf584b6c7e28900534b1ff7b8b48a66ae6ded
galadriellagoguadze <galadriellamedeiros999@gmail.com>
2026-06-22 15:59:09 -0300

    Update Restaurante.java

--- Restaurante.java
+++ Restaurante.java
@@ -21,19 +21,69 @@ public class Restaurante {
 	public void receberVoto() {
 		this.votos++;
 	}
+	
+	public int getVotos() {
+		return votos;
+	}
+
 	public String[] listarClientes() {
 		String[] lista = new String[clientes.size()];
 		int i = 0;
 		for(Cliente c : clientes) {
-			lista[i] = c.toString();
+			if(c.getPratoFav().equals(prato)) {
+				lista[i] = "Cliente: " + c.toString() + ", Prato preferido da casa";
+			}
+			else{
+				lista[i] = "Cliente: " + c.toString();
+			}
 			i++;
 		}
 		return lista;
 	}
-	public void adicionarCliente(Cliente c) {
-		this.clientes.add(c);
+	
+	public String[] listarVisitas() {
+		String[] lista = new String[visitas.size()];
+		int i = 0;
+		for(Visita v : visitas) {
+			lista[i] = v.toString();
+			i++;
+		}
+		return lista;
+	}
+	public String[] listarUltimasNVisitas(int n) {
+    	int tamanho = visitas.size();
+    	String[] lista = new String[n];
+    	int indiceArray = 0;
+        for (int i = tamanho - 1; i >= tamanho - n; i--) {
+            lista[indiceArray] = visitas.get(i).toString();
+            indiceArray++; 
+        }
+    	return lista;
 	}
 	
+
+	public boolean clienteVisitou(Cliente c) {
+		if(clientes.contains(c)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public void adicionarCliente(Cliente c) {
+		if(clienteVisitou(c)) {
+			return;
+		}
+		clientes.add(c);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public String getPrato() {
+		return prato;
+	}
+
 	public void registrarVisita(Visita v) {
 		this.visitas.add(v);
 	}
@@ -53,7 +103,13 @@ public class Restaurante {
 		Restaurante other = (Restaurante) obj;
 		return Objects.equals(nome, other.nome);
 	}
+	public int quantidadeClientes() {
+	    return clientes.size();
+	}
 
+	public int quantidadeVisitas() {
+	    return visitas.size();
+	}
 	@Override
 	public String toString() {
 		return "Restaurante [nome=" + nome + ", prato=" + prato + ", votos=" + votos + ", clientes=" + clientes
